b784a79b1990a51f497196178a66fe623a68e49f
florian90 <florian.staine@utbm.fr>
2015-12-22 19:12:03 +0100

    Attribut plutôt que héritage. La HashMap est maintenant un attribt, on n'a plus accès aux méthodes de hashMap comme ca. Ajout des méthodes add(PackRess) et remove(PackRess) pour utiliser un PackRess comme attribut dans le joueur.

--- src/model/joueur/PackRess.java
+++ src/model/joueur/PackRess.java
@@ -2,19 +2,21 @@ package model.joueur;
 
 import java.util.HashMap;
 
-public class PackRess extends HashMap<Ressource, Integer> {
+public class PackRess  {
+	private HashMap<Ressource, Integer> me;
 
 	public PackRess()
 	{
+		me = new HashMap<>();
 		for(Ressource r: Ressource.values())
 		{
-			put(r, 0);
+			me.put(r, 0);
 		}
 	}
 
 	public PackRess(Ressource... rs)
 	{
-		this();
+		me = new HashMap<>();
 		for(Ressource r : rs)
 		{
 			add(r);
@@ -24,37 +26,53 @@ public class PackRess extends HashMap<Ressource, Integer> {
 	public PackRess(PackRess pack)
 	{
 		this();
-		forEach((r,s)->set(r, pack.get(r)));
+		me.forEach((r,s)->set(r, pack.me.get(r)));
 	}
 
 	public void add(Ressource r)
 	{
-		put(r, get(r)+1);
+		me.put(r, me.get(r)+1);
 	}
 
 	public void add(Ressource r, int nbr)
 	{
-		put(r, get(r)+nbr);
+		me.put(r, me.get(r)+nbr);
 	}
 
 	public void remove(Ressource ress)
 	{
-		put(ress, get(ress)-1);
+		me.put(ress, me.get(ress)-1);
 	}
 
 	public void remove(Ressource r, int nbr)
 	{
-		put(r, get(r)-nbr);
+		me.put(r, me.get(r)-nbr);
 	}
 
 	public void set(Ressource r, int nbr)
 	{
-		put(r, nbr);
+		me.put(r, nbr);
 	}
 
 	public void mult(int factor)
 	{
 		/* Multiplie tous les champs par factor */
-		forEach((r, i) -> put(r,i*factor));
+		me.forEach((r, i) -> me.put(r,i*factor));
+	}
+
+	public void add(PackRess pack)
+	{
+		for(Ressource ressource : Ressource.values())
+		{
+			set(ressource, me.get(ressource) + pack.me.get(ressource));
+		}
+	}
+
+	public void remove(PackRess pack)
+	{
+		for(Ressource ressource : Ressource.values())
+		{
+			set(ressource, me.get(ressource) - pack.me.get(ressource));
+		}
 	}
 }
