5f506facd2fcbf7a4877e1ebcf9f374616de2e39
florian90 <florian.staine@utbm.fr>
2015-12-23 21:38:35 +0100

    Implémentation de Achetable Impléente la nouvelle interface piour tous les objets que l'on peut acheté (Je savais même pas qu'une enum pouvait implémenter une interfaace :o)

--- src/model/joueur/Invention.java
+++ src/model/joueur/Invention.java
@@ -1,10 +1,38 @@
 package model.joueur;
 
-public enum Invention
+public enum Invention implements Achetable
 {
-	TrainKiVol,
-	Train,
-	HoverBoard,
-	ConvecteurTemporel,
+	/*TrainKiVol,*/
+	Train
+			{
+				@Override
+				public PackRess cout()
+				{
+					return new PackRess(Ressource.Roue, 4, Ressource.Bois, 5);
+				}
+			},
+	HoverBoard
+			{
+				@Override
+				public PackRess cout()
+				{
+					return new PackRess(Ressource.Antenne, 2, Ressource.HautParleur, 4, Ressource.Metal, 3);
+				}
+			},
+	ConvecteurTemporel
+			{
+				@Override
+				public PackRess cout()
+				{
+					return new PackRess(Ressource.MorceauSchema, 3, Ressource.Plutonium, 6);
+				}
+			},
 	Radio
+			{
+				@Override
+				public PackRess cout()
+				{
+					return new PackRess(Ressource.Ventilateur, 4, Ressource.Aimant, 2, Ressource.Metal, 3);
+				}
+			}
 }
