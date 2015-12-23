a3bc9e5bf10614c9a7170fee3734761dd14b6b50
florian90 <florian.staine@utbm.fr>
2015-12-23 22:15:27 +0100

    Getterrs pour les 2 ressources spécifiques à l'époque

--- src/model/jeu/Epoque.java
+++ src/model/jeu/Epoque.java
@@ -1,9 +1,35 @@
 package model.jeu;
 
+import model.joueur.Ressource;
+
 public enum Epoque
 {
 	_1855,
 	_1955,
 	_1985,
-	_2015
+	_2015;
+
+	public static Ressource getR1(Epoque epoque)
+	{
+		switch (epoque)
+		{
+			case _1855: return Ressource.Bois;
+			case _1955: return Ressource.HautParleur;
+			case _1985: return Ressource.Plutonium;
+			case _2015: return Ressource.Aimant;
+		}
+		return null;
+	}
+
+	public static Ressource getR2(Epoque epoque)
+	{
+		switch (epoque)
+		{
+			case _1855: return Ressource.Roue;
+			case _1955: return Ressource.Antenne;
+			case _1985: return Ressource.MorceauSchema;
+			case _2015: return Ressource.Ventilateur;
+		}
+		return null;
+	}
 }
