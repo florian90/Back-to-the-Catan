bd253bf9b5046c09dd523fec5f995d62542e1947
florian90 <florian.staine@utbm.fr>
2015-12-23 22:15:51 +0100

    Implémente achetable

--- src/model/joueur/Carte.java
+++ src/model/joueur/Carte.java
@@ -1,6 +1,10 @@
 package model.joueur;
 
-public class Carte
+public class Carte implements Achetable
 {
-
+	public PackRess cout()
+	{
+		//FixMe: Mettre le cour réel
+		return new PackRess(Ressource.HautParleur, Ressource.Aimant, Ressource.Aimant, Ressource.Bois);
+	}
 }
