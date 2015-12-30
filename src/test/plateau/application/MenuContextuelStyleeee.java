621a860334136dac5c92680e175b233bcf85f6eb
florian90 <florian.staine@utbm.fr>
2015-12-30 22:02:55 +0100

    Ne sert pas enfaite... C'était à la base pour faire une interface qui s'affiche lorsque l'on clique sur le point et où l'on choisissait ce que qe l'on voulait construire mais il n'y a toujor qu'une possibilité enfaite...

--- src/test/plateau/application/MenuContextuelStyleeee.java
+++ src/test/plateau/application/MenuContextuelStyleeee.java
@@ -1,21 +1,21 @@
 package test.plateau.application;
 
 import javafx.scene.Group;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
-import vue.jeu.Constants;
-import vue.jeu.plateau.VuePoint;
 
-public class MenuContextuelStyleeee extends Group {
+public class MenuContextuelStyleeee extends Group {/*
+	Jeu m_jeu;
+	Fenetre m_fenetre;
+
 	Arc arc;
+	ImageView imageVille = new ImageView("textures/Ville.jpg");
+	ImageView imageVillage = new ImageView("textures/Village.jpg");
 
-	public MenuContextuelStyleeee(int x, int y)
+	public MenuContextuelStyleeee(Jeu jeu, Fenetre fenetre)
 	{
+		m_jeu = jeu;
+		m_fenetre = fenetre;
+
 		arc = new Arc();
-		arc.setCenterX(x);
-		arc.setCenterY(y);
 		arc.setRadiusX(Constants.arcWidth);
 		arc.setRadiusY(Constants.arcHeight);
 		arc.setStartAngle(-15);
@@ -23,17 +23,51 @@ public class MenuContextuelStyleeee extends Group {
 		arc.setType(ArcType.OPEN);
 		arc.setFill(Color.TRANSPARENT);
 		arc.setStroke(Color.RED);
-		getChildren().add(new VuePoint(null, x, y, Constants.pointRadius));
+		arc.setStrokeWidth(5);/*
+		imageVille.setFitWidth(50);
+		imageVille.setFitHeight(50);
+		imageVille.setOnMouseClicked((e)->{
+			System.out.println("Clicked on image");
+		});
+		imageVille.setOnMouseEntered((e)->{
+			System.out.println("Construire");
+		});
+		imageVillage.setOnMouseClicked((e)->{
+			System.out.println("Clicked on image");
+		});
+		imageVillage.setOnMouseEntered((e)->{
+			System.out.println("Construire");
+		});
+	}
 
-		ImageView imageVille = new ImageView("textures/Ville.jpg");
-		ImageView imageVillage = new ImageView("textures/Village.jpg");
+	public void showMenu(VuePoint vuePoint)
+	{
+		int x = vuePoint.getX();
+		int y = vuePoint.getY();
 
-		imageVillage.setX(x);
-		imageVillage.setY(y);
-		imageVillage.setFitWidth(50);
-		imageVillage.setFitHeight(50);
+		arc.setCenterX(x);
+		arc.setCenterY(y);
 
 		getChildren().add(arc);
-		getChildren().addAll(imageVillage);
-	}
+		if(vuePoint.getPoint().getType() == TypePoint.Vide)
+		{
+			imageVillage.setX(x);
+			imageVillage.setY(y);
+			imageVillage.setOnMouseClicked((e)->{
+				m_jeu.getJoueur().construirePoint(TypePoint.Village, vuePoint.getPoint());
+				getChildren().removeAll(arc, imageVillage);
+			});
+			getChildren().add(imageVillage);
+		}
+		if(vuePoint.getPoint().getType() == TypePoint.Village)
+		{
+			imageVille.setX(x);
+			imageVille.setY(y);
+			imageVille.setOnMouseClicked((e)->{
+				m_jeu.getJoueur().construirePoint(TypePoint.Ville, vuePoint.getPoint());
+				getChildren().removeAll(arc, imageVille);
+			});
+			getChildren().add(imageVille);
+		}
+	}*/
 }
