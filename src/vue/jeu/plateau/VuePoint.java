915728095024a64d6b22cd76043fd61886c64b21
florian90 <florian.staine@utbm.fr>
2015-12-30 22:00:06 +0100

    Extends group Possède un cercle à afficher uniquement si le point est de type vide Appelle la fonction dans jeu lors de la construction

--- src/vue/jeu/plateau/VuePoint.java
+++ src/vue/jeu/plateau/VuePoint.java
@@ -1,49 +1,60 @@
 package vue.jeu.plateau;
 
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import model.jeu.Jeu;
 import model.jeu.Point;
+import model.jeu.TypePoint;
+import vue.jeu.Constants;
+import vue.jeu.Fenetre;
 
-public class VuePoint extends Circle{
+public class VuePoint extends Group{
 	
 	private Point m_point;
+	private Circle m_cercle;
+
+	private Jeu m_jeu;
+	private Fenetre m_fenetre;
 	
-	public VuePoint(Point p, float centreX, float centreY, float rayon)
+	public VuePoint(Point p, float centreX, float centreY, Jeu jeu)
 	{
-		super(centreX,centreY,rayon);
 		m_point = p;
-		
-		setStroke(Color.BLACK);
-		setFill(Color.BLACK);
-		
-		setOnMouseEntered(new EventHandler<MouseEvent>() {
+		m_jeu = jeu;
+		m_fenetre = m_jeu.getFenetre();
 
-			@Override
-			public void handle(MouseEvent event) {
-				setFill(Color.WHITE);				
+		m_cercle = new Circle(centreX,centreY, Constants.pointRadius);
+		m_cercle.setStroke(Color.BLACK);
+		m_cercle.setFill(Color.BLACK);
+		if(p.getType() == TypePoint.Vide)
+		{
+			getChildren().add(m_cercle);
+		}
+
+		m_cercle.setOnMouseEntered((e)->{
+			m_cercle.setFill(Color.WHITE);
+			if(m_jeu.isDeLance())
+			{
+				if (m_jeu.isConstructionActrive())
+					m_fenetre.setStatus("Construire un nouveau point");
+				else
+					m_fenetre.setStatus("Cliquez sr le boutton construire si vous souhaitez construire un bâtiment ici");
 			}
 		});
-		
-		setOnMouseExited(new EventHandler<MouseEvent>() {
 
-			@Override
-			public void handle(MouseEvent event) {
-				setFill(Color.BLACK);				
-			}
+		m_cercle.setOnMouseExited((e)->{
+			m_fenetre.resetStatus();
+			m_cercle.setFill(Color.BLACK);
 		});
-		
-		setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+		m_cercle.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
-				System.out.println(m_point.toString());
-				setFill(Color.BLACK);
-				setStroke(Color.WHITE);
-				
+				m_jeu.clicPoint(m_point);
 			}
 		});
 	}
-
 }
