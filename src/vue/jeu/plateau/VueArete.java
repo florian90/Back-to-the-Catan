0ed70b85a6499037375e60844562cf5adbf463b8
florian90 <florian.staine@utbm.fr>
2016-01-02 12:32:14 +0100

    On peut construire sur les aretes Très similaire à vuePoint, on aurait pet-être pu grouper les deux, initialise les couleurs, taille, ... des objets à afficher plus fonction pdate lorsq'il y a eu un changement

--- src/vue/jeu/plateau/VueArete.java
+++ src/vue/jeu/plateau/VueArete.java
@@ -1,71 +1,103 @@
 package vue.jeu.plateau;
 
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import model.jeu.Arete;
 import model.jeu.Jeu;
+import model.jeu.TypeArete;
 import vue.jeu.Constants;
+import vue.jeu.Fenetre;
 
-public class VueArete extends Line
-{
+public class VueArete extends Group {
 	private Arete m_arete;
 	private Jeu m_jeu;
+	private Fenetre m_fenetre;
+
+	private Line m_ligne;
+	private Line m_ligneJoueur;
 	
 	public VueArete(Arete a, float debutX, float debutY, float finX, float finY, Jeu jeu)
 	{
-		super(debutX,debutY,finX,finY);
 		m_arete = a;
 		m_jeu = jeu;
-		
-		setStrokeWidth(Constants.roadWidth);
-		setStroke(Color.GRAY);
-		
-		setOnMouseEntered(new EventHandler<MouseEvent>() {
+		m_fenetre = jeu.getFenetre();
 
-			@Override
-			public void handle(MouseEvent event) {
-				//setStroke(Color.WHITE);
-				/*if(m_arete.peutEtreAutoroute())
-				{
-					setStroke(Color.YELLOW);	
-				}
+		m_ligne = new Line(debutX, debutY, finX, finY);
+		m_ligne.setStrokeWidth(Constants.roadWidth - 2);
+		m_ligne.setStroke(Color.GRAY);
+
+		m_ligneJoueur = new Line(debutX, debutY, finX, finY);
+		m_ligneJoueur.setStrokeWidth(Constants.roadWidth);
+
+		getChildren().addAll(m_ligneJoueur, m_ligne);
+
+		/***** Evènements sur les objets *****/
+		setOnMouseEntered((e) -> {
+			m_ligne.setStroke(Color.LIGHTGRAY);
+			m_ligneJoueur.setStroke(Color.LIGHTGRAY);
+			if (m_jeu.isDeLance())
+			{
+				if (m_jeu.isConstructionActive())
+					m_fenetre.setStatus("Construire une nouvelle arète");
 				else
-				{
-					setStroke(Color.BLUE);
-				}*/
-				
+					m_fenetre.setStatus("Cliquez sur le boutton construire si vous souhaitez construire une liaison ici");
 			}
 		});
-		
-		setOnMouseExited(new EventHandler<MouseEvent>() {
+		setOnMouseExited((e) -> {
+			m_fenetre.resetStatus();
+			update();
+		});
+		setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event)
+			{
+				m_jeu.clicArete(m_arete);
+			}
+		});
+
+		/***** Test sur les lignes *****/
+		/*
+		m_ligne.setOnMouseExited(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
 			//	setStroke(Color.GRAY);				
 			}
 		});
-		
-		setOnMouseClicked(new EventHandler<MouseEvent>() {
+		m_ligne.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
 				System.out.println(m_arete.toString());
 				if(m_arete.peutEtreAutoroute())
 				{
-					setStroke(Color.YELLOW);	
+					m_ligne.setStroke(Color.YELLOW);
 				}
 				else
 				{
-					setStroke(Color.BLUE);
+					m_ligne.setStroke(Color.BLUE);
 				}
-				
-			
 			}
-		});
-		
-		
+		});*/
+		update();
+	}
+
+	public void update()
+	{
+		if (m_arete.getType() == TypeArete.Route)
+			m_ligne.setStroke(Color.GRAY);
+		else if (m_arete.getType() == TypeArete.Autoroute)
+			m_ligne.setStroke(Color.BLACK);
+		else
+			m_ligne.setStroke(Color.GRAY);
+		if (m_arete.getProprietaire() != null)
+			m_ligneJoueur.setStroke(m_arete.getProprietaire().getCouleur());
+		else
+			m_ligneJoueur.setStroke(m_ligne.getStroke());
 	}
 
 }
